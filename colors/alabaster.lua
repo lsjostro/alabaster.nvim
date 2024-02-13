67fc9e08d4596df204fc9459e7ce478c16ab3dde
Brian Miranda <brianmiranda.dev@gmail.com>
2024-02-13 13:35:24 -0500

    :hammer: change background color back to solid color

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -140,7 +140,7 @@ if vim.o.background == "dark" then
 		MatchParen = { underline = 1, sp = active },
 		MoreMsg = { fg = nord.green, bold = 1 },
 		NonText = { fg = nord.bright_black },
-		Normal = { bg = "#000000", fg = fg },
+		Normal = { bg = bg, fg = fg },
 		NormalFloat = { bg = float_bg },
 		FloatBorder = floatborder,
 		Pmenu = { bg = pmenu_bg },
